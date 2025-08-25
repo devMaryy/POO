a505b00f75ad19f29b0420bb26356dde37fd2d05
Maria Eduarda <duda.mariaa28@gmail.com>
2025-08-25 18:09:58 -0300

    Add files via upload

---
+++ prjExemplo3/src/Exemplo3.java
@@ -0,0 +1,17 @@
+/**
+ * @author Maria Simões
+ */
+
+public class Exemplo3 {
+
+    public static void main(String[] args) {
+        
+        int cont;
+        
+        System.out.println("\tTABUADA DO SETE\n");
+        
+        for (cont = 1; cont <= 10; cont++) {
+            System.out.println("7 * " + cont + " = " + cont * 7);
+        }     
+    }
+}
