948cd9e9f63ba76e234076168a6259f471423615
Maria Eduarda <duda.mariaa28@gmail.com>
2025-08-25 18:05:05 -0300

    Add files via upload

---
+++ prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,28 @@
+// Comentário em linha
+
+/**
+ * Coemtário em bloco
+ * @author Maria Simoes
+ */
+public class Exemplo1 {
+
+    public static void main(String[] args) {
+        
+        int idade;
+        double peso;
+        char sexo;
+        String nome;
+        
+        idade = 34;
+        peso = 78.5;
+        sexo = 'F';
+        nome = "Ana Beatriz";
+        
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Nome: " + nome);
+        
+    }
+    
+}
